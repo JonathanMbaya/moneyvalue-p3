089439f5887d74bd6b1065d43d1494fc5dabb6d0
NateDev <jonathanmbaya13@gmail.com>
2023-11-09 02:15:22 +0100

    Branchement de vue-js au proej

--- moneyvalue-p3/resources/js/app.js
+++ moneyvalue-p3/resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+
+import {createApp} from "vue";
+
+import DevisesList from "./components/devises/DevisesList.vue";
+
+
+const app = createApp();
+
+app.component('DevisesList', DevisesList);
+
+app.mount('#app')
